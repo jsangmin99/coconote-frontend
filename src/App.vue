471ef0fa9353acbecc08da75a0c7b1ad56564c4c
sseho <kindseho@naver.com>
2024-10-24 12:27:36 +0900

    [Feat] HC-218 로딩 추가

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,8 @@
           </div>
         </v-col>
         <v-col :cols="showHeaderAndSidebar ? 10 : 12">
-          <v-sheet class="main-content">
+          <LoadingComponent v-if="loading" />
+          <v-sheet v-if="!loading" class="main-content">
             <router-view :key="$route.fullPath" />
           </v-sheet>
         </v-col>
@@ -29,6 +30,7 @@
 <script>
 import CommonTopMenu from "@/components/basic/CommonTopMenu.vue";
 import InnerMenu from "@/components/basic/InnerMenu.vue";
+import LoadingComponent from "./components/basic/LoadingComponent.vue";
 
 export default {
   computed: {
@@ -54,11 +56,20 @@ export default {
   components: {
     CommonTopMenu,
     InnerMenu,
+    LoadingComponent,
   },
   data() {
     return {
       workspaceInfo: [],
+      loading: true,
     };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      console.log("전체화면로드 완료");
+      
+      this.loading = false
+    });
   },
   methods: {},
 };
